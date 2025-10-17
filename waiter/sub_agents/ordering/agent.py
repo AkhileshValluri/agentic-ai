095c416dfdfd632c0fa32aaf285e0747f0ebd3be
Akhilesh Kaushik Valluri <akhilesh.k.valluri@gmail.com>
2025-10-17 14:05:47 +0000

    changed recommendataion agent into factory and added link between recommendation agent and ordering agent

--- waiter/sub_agents/ordering/agent.py
+++ waiter/sub_agents/ordering/agent.py
@@ -4,7 +4,7 @@ from google.adk.agents import LlmAgent
 from waiter.sub_agents.ordering import prompt
 from waiter.models.services import OrderService
 from waiter.tools.memory import order_model_init
-from waiter.sub_agents.recommendation.agent import recommendations_refinement_loop_agent
+from waiter.sub_agents.recommendation.agent import instantiate_refinement_loop_agent
 
 ordering_agent = LlmAgent(
     model="gemini-2.0-flash",
@@ -12,6 +12,6 @@ ordering_agent = LlmAgent(
     description="Agent which takes a customers order and places the order",
     instruction=prompt.order_agent_instr,
     tools=[OrderService.get_dishes, OrderService.update_dishes, OrderService.place_order],
-    sub_agents=[recommendations_refinement_loop_agent],
+    sub_agents=[instantiate_refinement_loop_agent()],
     before_agent_callback=order_model_init
 )
--- waiter/sub_agents/recommendation/agent.py
+++ waiter/sub_agents/recommendation/agent.py
@@ -16,34 +16,70 @@ def exit_if_perfect(tool_context: ToolContext):
     tool_context.actions.escalate = True
     tool_context.actions.skip_summarization = True
 
-recommendation_agent = Agent(
-    model="gemini-2.0-flash",
-    name="recommendation_agent",
-    description="Handles the recommendation, possible modifications, checking of dishes as per user query.",
-    instruction=prompt.recommendation_agent_instr,
-    tools=[
-        DishStore.request_modification,
-    ],
-    output_key=constants.INITIAL_RECOMMENDATION_KEY
-)
+# recommendation_agent = Agent(
+#     model="gemini-2.0-flash",
+#     name="recommendation_agent",
+#     description="Handles the recommendation, possible modifications, checking of dishes as per user query.",
+#     instruction=prompt.recommendation_agent_instr,
+#     tools=[
+#         DishStore.request_modification,
+#     ],
+#     output_key=constants.INITIAL_RECOMMENDATION_KEY
+# )
 
-critique_agent = Agent(
-    model="gemini-2.0-flash",
-    name="critique_agent",
-    description="Critiques the recommendation based off of the ingredients and the allergies and the preferences that the user has",
-    instruction=prompt.critique_agent_instr,
-    tools=[
-        DishStore.request_modification,
-        RecommendationService.save_recommendation,
-        exit_if_perfect,
-    ],
-    output_key=constants.INITIAL_CRITIQUE_KEY
-)
+# critique_agent = Agent(
+#     model="gemini-2.0-flash",
+#     name="critique_agent",
+#     description="Critiques the recommendation based off of the ingredients and the allergies and the preferences that the user has",
+#     instruction=prompt.critique_agent_instr,
+#     tools=[
+#         DishStore.request_modification,
+#         RecommendationService.save_recommendation,
+#         exit_if_perfect,
+#     ],
+#     output_key=constants.INITIAL_CRITIQUE_KEY
+# )
 
-recommendations_refinement_loop_agent = LoopAgent(
-    name="recommendations_refinement_loop_agent",
-    description="Handles all the recommendations, modifications of the dishes as per customers requirement",
-    before_agent_callback=recommendation_model_init, # init recom object for guest
-    sub_agents=[recommendation_agent, critique_agent],
-    max_iterations=5
-)
+# recommendations_refinement_loop_agent = LoopAgent(
+#     name="recommendations_refinement_loop_agent",
+#     description="Handles all the recommendations, modifications of the dishes as per customers requirement",
+#     before_agent_callback=recommendation_model_init, # init recom object for guest
+#     sub_agents=[recommendation_agent, critique_agent],
+#     max_iterations=5
+# )
+
+def instantiate_refinement_loop_agent(): 
+    recommendation_agent = Agent(
+        model="gemini-2.0-flash",
+        name="recommendation_agent",
+        description="Handles the recommendation, possible modifications, checking of dishes as per user query.",
+        instruction=prompt.recommendation_agent_instr,
+        tools=[
+            DishStore.request_modification,
+        ],
+        output_key=constants.INITIAL_RECOMMENDATION_KEY
+    )
+
+    critique_agent = Agent(
+        model="gemini-2.0-flash",
+        name="critique_agent",
+        description="Critiques the recommendation based off of the ingredients and the allergies and the preferences that the user has",
+        instruction=prompt.critique_agent_instr,
+        tools=[
+            DishStore.request_modification,
+            RecommendationService.save_recommendation,
+            exit_if_perfect,
+        ],
+        output_key=constants.INITIAL_CRITIQUE_KEY
+    )
+
+    recommendations_refinement_loop_agent = LoopAgent(
+        name="recommendations_refinement_loop_agent",
+        description="Handles all the recommendations, modifications of the dishes as per customers requirement",
+        before_agent_callback=recommendation_model_init, # init recom object for guest
+        sub_agents=[recommendation_agent, critique_agent],
+        max_iterations=5
+    )
+    return recommendations_refinement_loop_agent
+
+recommendations_refinement_loop_agent = instantiate_refinement_loop_agent()
